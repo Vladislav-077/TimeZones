3e5b06a7589c7c421987fbf0d45781af5a25301e
Vladislav Abovyan <vladislav.abovian@aisa.ru>
2021-01-30 23:40:33 +0300

    master: git - INIT

---
+++ src/test/java/com/example/TimeZones/TimeZonesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TimeZones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeZonesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
