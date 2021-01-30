5221fefd46561638039dead94160458fbf2cfe53
Vladislav Abovyan <vladislav.abovian@aisa.ru>
2021-01-31 01:42:56 +0300

    fixed

--- src/main/java/com/example/TimeZones/MyUI.java
+++ src/main/java/com/example/TimeZones/MyUI.java
@@ -1,13 +1,15 @@
 package com.example.TimeZones;
 
 import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.Title;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
 @SpringUI
-@Theme("valo")
+@Title(value = "Время")
+@Theme("mytheme")
 public class MyUI extends UI {
     private VerticalLayout verticalLayout;
 
