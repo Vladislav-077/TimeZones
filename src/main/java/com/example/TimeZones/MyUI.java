c59cfcae7d0a5bde3250d12d19986c23246188bc
Vladislav Abovyan <vladislav.abovian@aisa.ru>
2021-01-31 00:46:42 +0300

    develop: complite start application

--- src/main/java/com/example/TimeZones/MyUI.java
+++ src/main/java/com/example/TimeZones/MyUI.java
@@ -1,4 +1,24 @@
 package com.example.TimeZones;
 
-public class UI {
+import com.vaadin.annotations.Theme;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+@SpringUI
+@Theme("valo")
+public class MyUI extends UI {
+    private VerticalLayout verticalLayout;
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+        setupLayout(); // добавили вертикальный слой.
+
+    }
+
+    private void setupLayout(){
+        verticalLayout = new VerticalLayout();
+        setContent(verticalLayout);
+    }
 }
